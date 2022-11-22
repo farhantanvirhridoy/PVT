e99a20591d19719d03eea0c1b0493ae854128678
Farhan Tanvir Hridoy <tanvirridoy864@gmail.com>
2022-11-22 19:20:44 +0600

    add

---
+++ flowrate_test_1.ino
@@ -0,0 +1,28 @@
+const int input = A0;
+
+float get_flowrate() {
+  int X;
+  int Y;
+  float TIME = 0;
+  float FREQUENCY = 0;
+  float WATER = 0;
+  float LS = 0;
+  X = pulseIn(input, HIGH);
+  Y = pulseIn(input, LOW);
+  TIME = X + Y;
+  FREQUENCY = 1000000 / TIME;
+  WATER = FREQUENCY / 7.5;
+  LS = WATER / 60;
+  return LS;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(get_flowrate());
+  Serial.println(" L/h");
+}
